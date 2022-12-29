aa2869e5a049ef7521f4177c801b5ba459b9eee7
rodina12 <111309281+rodina12@users.noreply.github.com>
2022-12-28 20:09:29 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,20 @@
 
-var navMenu = document.querySelectorAll("nav");
-// make smooth scroll
-navMenu.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    var Id = event.target.attributes.href.value;
-    var sec = document.querySelector(Id);
-    sec.scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
-
+//add active state to section 
 var offset = (section) => {
   return Math.floor(section.getBoundingClientRect().top);
-}; // remove active class
+}; 
 
-var remove = (section) => {
-  section.classList.remove("your-active-class");
-  section.style =
-    "background-color:gray";
-}; // add active class
 var add = (x, section) => {
   if (x) {
     section.classList.add("your-active-class");
-    section.style = "background-color: blue;";
+    section.style.background="red";
   }
 }; 
+var remove = (section) => {
+  section.classList.remove("your-active-class");
+  section.style.background="gray";
+}; 
+
 var sec = document.querySelectorAll("section");
 var Activation = () => {
   sec.forEach((section) => {
@@ -37,3 +25,19 @@ var Activation = () => {
   });
 };
 window.addEventListener("scroll", Activation);
+
+// make smooth scroll
+
+var navMenu = document.querySelectorAll("nav");
+const element =document.createElement("li");
+
+navMenu.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    var Id = event.target.attributes.href.value;
+    var sec = document.querySelector(Id);
+    sec.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
